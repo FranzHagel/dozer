e98ca041506bf1afac9bdb436cf835000aaf87a6
FranzHagel <40021634+FranzHagel@users.noreply.github.com>
2018-06-07 10:52:39 +0200

    Update DozerConverter.java
    Why is the comparison of classes first made with equals(), if isAssignable() also returns true if same?

--- core/src/main/java/com/github/dozermapper/core/DozerConverter.java
+++ core/src/main/java/com/github/dozermapper/core/DozerConverter.java
@@ -44,15 +44,7 @@ public abstract class DozerConverter<A, B> implements ConfigurableCustomConverte
         Class<?> wrappedDestinationClass = ClassUtils.primitiveToWrapper(destinationClass);
         Class<?> wrappedSourceClass = ClassUtils.primitiveToWrapper(sourceClass);
 
-        if (prototypeA.equals(wrappedDestinationClass)) {
-            return convertFrom((B)sourceFieldValue, (A)existingDestinationFieldValue);
-        } else if (prototypeB.equals(wrappedDestinationClass)) {
-            return convertTo((A)sourceFieldValue, (B)existingDestinationFieldValue);
-        } else if (prototypeA.equals(wrappedSourceClass)) {
-            return convertTo((A)sourceFieldValue, (B)existingDestinationFieldValue);
-        } else if (prototypeB.equals(wrappedSourceClass)) {
-            return convertFrom((B)sourceFieldValue, (A)existingDestinationFieldValue);
-        } else if (prototypeA.isAssignableFrom(wrappedDestinationClass)) {
+        if (prototypeA.isAssignableFrom(wrappedDestinationClass)) {
             return convertFrom((B)sourceFieldValue, (A)existingDestinationFieldValue);
         } else if (prototypeB.isAssignableFrom(wrappedDestinationClass)) {
             return convertTo((A)sourceFieldValue, (B)existingDestinationFieldValue);
